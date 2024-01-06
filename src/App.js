92d28714b7b54966a342f9db7297c5f9ee524070
AnasKestraoui <akestraoui@gmail.com>
2024-01-06 15:08:07 +0100

    m

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React,{useState} from "react"
-import {hashRouter as Router,Routes,Route} from "react-router-dom"
+import {HashRouter as Router,Routes,Route} from "react-router-dom"
 import Authentification from "./Projet_location/Authentification/Authentification"
 import Home from "./Projet_location/Home/Home"
 import 'bootstrap/dist/css/bootstrap.min.css'
